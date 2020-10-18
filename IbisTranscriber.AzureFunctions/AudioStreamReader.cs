dd927c92f5f9808c80c1313c753affc8e72650a2
Antonio Correia <antonio.alves.correia@gmail.com>
2020-10-18 01:45:01 -0300

    azure functions project

---
+++ IbisTranscriber.AzureFunctions/AudioStreamReader.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IbisTranscriber.AzureFunctions
+{
+    class AudioStreamReader : PullAudioInputStreamCallback
+    {
+        private BinaryReader _reader;
+        public AudioStreamReader(Stream stream)
+        {
+            _reader = new BinaryReader(stream);
+        }
+        public override int Read(byte[] dataBuffer, uint size)
+        {
+            return _reader.Read(dataBuffer, 0, (int)size);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+                
+
+            if (disposing)
+            {
+                _reader.Dispose();
+            }
+                
+
+            _disposed = true;
+
+            base.Dispose(disposing);
+        }
+
+        private bool _disposed = false;
+    }
+}
---
+++ IbisTranscriber.AzureFunctions/TriggerSpeechToText.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace IbisTranscriber.AzureFunctions
+{
+    public static class TriggerSpeechToText
+    {
+        [FunctionName("TriggerSpeechToText")]
+        public static async Task Run([BlobTrigger("projects/{name}.wav", Connection = "AzureWebJobsStorage")]Stream myBlob, 
+            [Blob("transcribedfiles/{name}.txt",FileAccess.Write, Connection = "TextStorage")] Stream textBlob,
+            string name, ILogger log)
+        {
+            var config = SpeechConfig.FromSubscription(Environment.GetEnvironmentVariable("ApiKey", EnvironmentVariableTarget.Process),
+                Environment.GetEnvironmentVariable("Region", EnvironmentVariableTarget.Process));
+
+            config.RequestWordLevelTimestamps();
+            //string fromLanguage = "en-US";
+            //config.SpeechRecognitionLanguage = fromLanguage;
+
+            //config.OutputFormat = OutputFormat.Simple;
+
+
+            var completionSource = new TaskCompletionSource<int>();
+
+            using(var audioInput = AudioConfig.FromStreamInput(new AudioStreamReader(myBlob)))
+            using (var recognizer = new SpeechRecognizer(config, audioInput))
+            {
+                var streamWriter = new StreamWriter(textBlob);
+
+                recognizer.Recognized += (s, e) =>
+                {
+                    //log.LogInformation("Recognized");
+                    streamWriter.Write(e.Result);
+                };
+
+
+
+                recognizer.SessionStopped += (s, e) =>
+                {
+                    //log.LogInformation("session stopped");
+                    streamWriter.Flush();
+                    streamWriter.Dispose();
+                    completionSource.TrySetResult(0);
+                };
+
+                await recognizer.StartContinuousRecognitionAsync()
+                    .ConfigureAwait(false);
+
+                await Task.WhenAny(new[] { completionSource.Task });
+                //log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+                await recognizer.StopContinuousRecognitionAsync()
+                    .ConfigureAwait(false);
+
+
+            }
+            
+        }
+
+       
+    }
+}
