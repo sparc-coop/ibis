40ea94a9fcb4426565b4b5bed4794793860695c0
Margaret Retzinger <maretzinger@gmail.com>
2022-02-17 16:27:03 -0500

    Adding new projects for version 2 setup

---
+++ Ibis.Features/_Plugins/IbisContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ibis.Features._Plugins
+{
+    public class IbisContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+
+        public IbisContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           
+            builder.Entity<Project>().HasPartitionKey(x => x.Id);
+
+        }
+    }
+}
---
+++ Ibis.Web/Program.cs
@@ -0,0 +1,17 @@
+using Ibis.Features;
+using Ibis.Web;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sparc.Authentication.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.AddB2CApi<IbisApi>(
+        "https://ibis.onmicrosoft.com/ccba7246-6276-4566-a964-12d7a2b48198/KodekitAPI.Access",
+        builder.Configuration["ApiUrl"]);
+
+await builder.Build().RunAsync();
