83a80899a8c78857a614b7294fd69f2a33770660
Margaret Retzinger <maretzinger@gmail.com>
2022-02-17 16:27:03 -0500

    Adding new projects for version 2 setup

---
+++ Ibis.Features/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Sparc.Authentication.AzureADB2C;
+using Sparc.Core;
+using Sparc.Features;
+using Sparc.Plugins.Database.Cosmos;
+
+namespace Ibis.Features
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Sparcify<Startup>()
+                //.AddCosmos<IbisContext>(Configuration.GetConnectionString("Database"), "ibis")
+                .AddAzureADB2CAuthentication(Configuration);
+
+            services.AddScoped(typeof(IRepository<>), typeof(CosmosDbRepository<>));
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Sparcify<Startup>(env);
+            app.UseDeveloperExceptionPage();
+        }
+    }
+}
---
+++ Ibis.Features/Users/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿using Sparc.Core;
+using Newtonsoft.Json;
+
+namespace Ibis.Features;
+public class User : Root<string>
+{
+    public User()
+    {
+        Id = string.Empty;
+        UserId = Id;
+    }
+
+    public string UserId { get { return Id; } set { Id = value; } }
+    private string? _email;
+    public string? Email
+    {
+        get { return _email; }
+        set
+        {
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                _email = null;
+                return;
+            }
+
+            _email = value.Trim().ToLower();
+        }
+    }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? DisplayName { get; set; }
+    [JsonIgnore]
+    public string FullName => $"{FirstName} {LastName}";
+    public DateTime DateCreated { get; set; }
+    public DateTime DateModified { get; set; }
+}
