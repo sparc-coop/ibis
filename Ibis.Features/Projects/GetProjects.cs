968e0d6ab5e7e9f974ba27ed81d27f4b49d9fc6d
Margaret Retzinger <maretzinger@gmail.com>
2022-03-08 11:51:55 -0500

    Refactoring and new pages

---
+++ Ibis.Features/Projects/GetProjects.cs
@@ -0,0 +1,25 @@
+﻿using Sparc.Core;
+using Sparc.Features;
+
+
+namespace Ibis.Features
+{
+    public record GetProjectResponse(string ProjectId, string ProjectName, string ProjectUrl, string UserId);
+    public class GetProjects : PublicFeature<string, List<GetProjectResponse>>
+    {
+        public IRepository<Project> Projects{ get; }
+        public GetProjects(IRepository<Project> projects)
+        {
+            Projects = projects;
+        }
+
+        public override async Task<List<GetProjectResponse>> ExecuteAsync(string userId)
+        {
+            List<Project> projectList = await Projects.Query.Where(x => x.UserID == userId).ToListAsync();
+            if (projectList == null)
+                throw new NotFoundException($"No projects found!");
+
+            return new List<GetProjectResponse>();
+        }
+    }
+}
