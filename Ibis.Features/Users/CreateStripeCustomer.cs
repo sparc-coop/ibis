e6d42c47165b19dbc44d8f029b59b5e5f92ac161
Margaret Retzinger <maretzinger@gmail.com>
2022-03-10 16:27:08 -0500

    Page updates

---
+++ Ibis.Features/Users/CreateStripeCustomer.cs
@@ -0,0 +1,40 @@
+﻿using Sparc.Core;
+using Sparc.Features;
+using Stripe;
+
+namespace Ibis.Features.Users
+{
+    public class CreateStripeCustomer : Feature<User, bool>
+    {
+        public IConfiguration? Configuration;
+
+        public override async Task<bool> ExecuteAsync(User request)
+        {
+            try
+            {
+                StripeConfiguration.ApiKey = Configuration["Strip:ApiKey"];
+
+                var options = new CustomerCreateOptions { };
+                var service = new CustomerService();
+                var customer = service.Create(options);
+                return true;
+            } catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        private void FutureBilling(string customerId)
+        {
+            StripeConfiguration.ApiKey = Configuration["Strip:ApiKey"];
+            var options = new SetupIntentCreateOptions
+            {
+                Customer = customerId,
+                PaymentMethodTypes = new List<string> { "card" },
+            };
+            var service = new SetupIntentService();
+            service.Create(options);
+        }
+    }
+}
--- Ibis.Web/wwwroot/js/site.js
+++ Ibis.Web/wwwroot/js/site.js
@@ -1,19 +1,15 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
-
-document.addEventListener('DOMContentLoaded', function (event) {
+﻿
+function textDisplay() {
     // array with texts to type in typewriter
     var dataText = ["Podcast", "Audio", "Video"];
 
     // type one text in the typwriter
     // keeps calling itself until the text is finished
     function typeWriter(text, i, fnCallback) {
-        // chekc if text isn't finished yet
+        // check if text isn't finished yet
         if (i < (text.length)) {
             // add next character to h1
-            document.querySelector(".typetext").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true" class="blink">|</span>';
+            document.getElementById("displaytext").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true" class="blink">|</span>';
 
             // wait for a while and call this function again for next character
             setTimeout(function () {
@@ -23,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
         // text finished, call callback if there is a callback function
         else if (typeof fnCallback == 'function') {
             // call callback after timeout
-            setTimeout(fnCallback, 700);
+            setTimeout(fnCallback, 840);
         }
     }
     // start a typewriter animation for a text in the dataText array
@@ -31,38 +27,21 @@ document.addEventListener('DOMContentLoaded', function (event) {
         if (typeof dataText[i] == 'undefined') {
             setTimeout(function () {
                 StartTextAnimation(0);
-            }, 20000);
+            }, 5000);
         }
+
         // check if dataText[i] exists
-        if (i < dataText[i].length) {
-            // text exists! start typewriter animation
-            typeWriter(dataText[i], 0, function () {
-                // after callback (and whole text has been animated), start next text
-                StartTextAnimation(i + 1);
-            });
+        if (dataText[i] != undefined) {
+            if (i < dataText[i].length) {
+
+                // text exists! start typewriter animation
+                typeWriter(dataText[i], 0, function () {
+                    // after callback (and whole text has been animated), start next text
+                    StartTextAnimation(i + 1);
+                });
+            }
         }
     }
     // start the text animation
     StartTextAnimation(0);
-});
-
-
-
-//"use strict";
-
-//var connection = new signalR.HubConnectionBuilder().withUrl("/notificationhub").build();
-
-//connection.on("newMessage", function (message) {
-//    var msg = message.replace(/&/g, "&").replace(/</g, "<").replace(/>/g, ">");
-//    var li = document.createElement("li");
-//    li.textContent = msg;
-//    document.getElementById("messagesList").appendChild(li);
-//});
-
-//connection.start().then(function () {
-//    var li = document.createElement("li");
-//    li.textContent = "Conectado!";
-//    document.getElementById("messagesList").appendChild(li);
-//}).catch(function (err) {
-//    return console.error(err.toString());
-//});
+};
