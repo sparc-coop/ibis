eed0621322f1f4a41ef70cff6da6489f484e4209
Margaret Retzinger <maretzinger@gmail.com>
2022-06-10 10:42:40 -0400

    Modal updates for edit and delete

---
+++ Ibis.Features/Rooms/UpdateRoomOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace Ibis.Features;
+
+public record RoomOptionsRequest(string roomId, string Title);
+public class UpdateRoomOptions : Feature<RoomOptionsRequest, bool>
+{ 
+    public UpdateRoomOptions(IRepository<Room> rooms)
+    {
+        Rooms = rooms;
+    }
+    public IRepository<Room> Rooms { get; }
+
+
+    public override async Task<bool> ExecuteAsync(RoomOptionsRequest request)
+    {
+        Room room = await Rooms.FindAsync(request.roomId);
+        room.Name = request.Title;
+        await Rooms.UpdateAsync(room);
+        return true;
+    }
+}
