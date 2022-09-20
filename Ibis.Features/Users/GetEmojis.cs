758fa1af27c8293b48b2eadb1f372b013c2f05a3
Joseph Young <joseph@josephayoung.com>
2022-09-20 12:14:11 -0500

    Add emoji selector

---
+++ Ibis.Features/Users/GetEmojis.cs
@@ -0,0 +1,11 @@
+﻿namespace Ibis.Features.Users;
+
+public record GetEmojisResponse(List<string> Skintones, List<string> Emojis);
+public class GetEmojis : Feature<GetEmojisResponse>
+{
+    public override Task<GetEmojisResponse> ExecuteAsync()
+    {
+        GetEmojisResponse result = new(UserAvatar.SkinTones(), UserAvatar.Emojis());
+        return Task.FromResult(result);
+    }
+}
