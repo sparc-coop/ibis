8039f341ee787d16d8baab924983f5366298f736
Joseph Young <joseph@josephayoung.com>
2022-10-03 17:32:17 -0500

    Tighten up event handling for messages

--- Ibis.Features/Rooms/JoinRoom.cs
+++ Ibis.Features/Rooms/JoinRoom.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.SignalR;
-
-namespace Ibis.Features.Rooms;
+﻿namespace Ibis.Features.Rooms;
 
 public record JoinRoomRequest(string RoomId);
 public record GetRoomResponse
@@ -10,16 +8,14 @@ public record GetRoomResponse
     public DateTime StartDate { get; private set; }
     public string Name { get; private set; }
     public List<UserAvatar>? Users { get; set; }
-    public List<Message>? Messages { get; set; }
 
-    public GetRoomResponse(Room room, List<Message>? messages = null)
+    public GetRoomResponse(Room room)
     {
         RoomId = room.Id;
         LastActiveDate = room.LastActiveDate;
         StartDate = room.StartDate;
         Name = room.Name;
         Users = room.Users;
-        Messages = messages;
     }
 }
 
@@ -45,13 +41,7 @@ public class JoinRoom : Feature<JoinRoomRequest, GetRoomResponse>
 
         await Users.ExecuteAsync(User.Id(), user => user.JoinRoom(request.RoomId));
         await Rooms.ExecuteAsync(request.RoomId, room => room.AddActiveUser(user));
-
-        var messages = await Messages
-                .Query
-                .Where(message => message.RoomId == request.RoomId && message.Language == user.Avatar.Language)
-                .OrderBy(x => x.Timestamp)
-                .ToListAsync();
-
-        return new(room, messages);
+        
+        return new(room);
     }
 }
