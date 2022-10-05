61a16ce0fa4f410fc572aa10aa41dea349b48d93
Joseph Young <joseph@josephayoung.com>
2022-10-05 10:27:50 -0500

    Add more robust watch mechanism for Signalr

--- Ibis.Features/Sparc.Realtime/SparcHub.cs
+++ Ibis.Features/Sparc.Realtime/SparcHub.cs
@@ -7,6 +7,12 @@ namespace Sparc.Realtime;
 [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 public class SparcHub : Hub
 {
+    public override async Task OnConnectedAsync()
+    {
+        if (Context.UserIdentifier != null)
+            await Clients.User(Context.UserIdentifier).SendAsync("_UserConnected");
+    }
+
     public virtual async Task Watch(string groupId)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
