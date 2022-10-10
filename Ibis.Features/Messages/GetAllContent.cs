99e3bc49fd5b7251a6209556608ab1cc8df35619
Joseph Young <joseph@josephayoung.com>
2022-10-10 15:36:31 -0500

    Rename GetSlackMessages -> GetContent and small architecture changes for genericization

---
+++ Ibis.Features/Messages/GetAllContent.cs
@@ -0,0 +1,26 @@
+﻿namespace Ibis.Features.Messages;
+public record GetAllContentRequest(string RoomId, string? Language, string ContentType = "Text");
+public record GetContentResponse(string Tag, string Text, string? Audio);
+public class GetAllContent : PublicFeature<GetAllContentRequest, List<GetContentResponse>>
+{
+    public GetAllContent(IRepository<Message> messages)
+    {
+        Messages = messages;
+    }
+    public IRepository<Message> Messages { get; }
+
+    public override async Task<List<GetContentResponse>> ExecuteAsync(GetAllContentRequest request)
+    {
+        List<Message> postList = await Messages.Query.Where(x => x.RoomId == request.RoomId && x.Text != null)
+            .OrderByDescending(y => y.Timestamp)
+            .ToListAsync();
+
+        List<GetContentResponse> result = new();
+        foreach (var item in postList)
+        {
+            result.Add(new(item.Tag ?? item.Id, item.Text!, item.Audio?.Url));
+        }
+
+        return result;
+    }
+}
---
+++ Ibis.Features/Messages/GetContent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ibis.Features.Messages;
+
+public record GetContentRequest(string RoomId, string Tag, string? Language, string? ContentType = "Text");
+public class GetContent : PublicFeature<GetContentRequest, GetContentResponse>
+{
+    public GetContent(IRepository<Message> messages)
+    {
+        Messages = messages;
+    }
+    public IRepository<Message> Messages { get; }
+
+    public override async Task<GetContentResponse> ExecuteAsync(GetContentRequest request)
+    {
+        var message = await Messages.Query
+            .Where(x => x.SiteName == request.RoomId && x.Tag == request.Tag)
+            .OrderByDescending(y => y.Timestamp)
+            .FirstOrDefaultAsync();
+
+        if (message == null)
+            throw new NotFoundException($"Message {request.Tag} not found!");
+
+        return new(message.Tag!, message.Text!, message.Audio?.Url);
+    }
+}
