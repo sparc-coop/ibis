fb8c27cc3fd4eb0a1144122adc40ae2045c28aef
Joseph Young <joseph@josephayoung.com>
2022-10-10 15:35:50 -0500

    Remove accidentally added files from merge

--- Ibis.Features/Sparc.Realtime/UserNotification.cs
+++ Ibis.Features/Sparc.Realtime/UserNotification.cs
@@ -1,20 +1,3 @@
-﻿using Microsoft.AspNetCore.SignalR;
-
-namespace Ibis.Features.Sparc.Realtime;
+﻿namespace Ibis.Features.Sparc.Realtime;
 
 public record UserNotification(string UserId) : INotification;
-
-public class UserNotificationForwarder : RealtimeFeature<UserNotification>
-{
-    public UserNotificationForwarder(IHubContext<RoomHub> hub)
-    {
-        Hub = hub;
-    }
-
-    public IHubContext<RoomHub> Hub { get; }
-
-    public override async Task ExecuteAsync(UserNotification notification)
-    {
-        await Hub.Clients.Client(notification.UserId).SendAsync(notification.GetType().Name, notification);
-    }
-}
