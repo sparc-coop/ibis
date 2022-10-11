aeddddb01947c97f845fa7f8cba41c03d171f5ff
Joseph Young <joseph@josephayoung.com>
2022-10-11 11:28:32 -0500

    Use slugs for get content requests

--- Ibis.Features/Messages/GetAllContent.cs
+++ Ibis.Features/Messages/GetAllContent.cs
@@ -1,18 +1,26 @@
 ﻿namespace Ibis.Features.Messages;
-public record GetAllContentRequest(string RoomId, string Language);
+public record GetAllContentRequest(string RoomSlug, string Language);
 public record GetContentResponse(string Tag, string Text, string? Audio);
 public class GetAllContent : PublicFeature<GetAllContentRequest, List<GetContentResponse>>
 {
-    public GetAllContent(IRepository<Message> messages)
+    public GetAllContent(IRepository<Message> messages, IRepository<Room> rooms)
     {
         Messages = messages;
+        Rooms = rooms;
     }
     public IRepository<Message> Messages { get; }
+    public IRepository<Room> Rooms { get; }
 
     public override async Task<List<GetContentResponse>> ExecuteAsync(GetAllContentRequest request)
     {
+        var room = Rooms.Query.FirstOrDefault(x => x.Slug == request.RoomSlug);
+        if (room == null)
+        {
+            throw new NotFoundException($"Room {request.RoomSlug} not found");
+        }
+
         List<Message> postList = await Messages.Query
-            .Where(x => x.RoomId == request.RoomId && x.Language == request.Language && x.Text != null)
+            .Where(x => x.RoomId == room.Id && x.Language == request.Language && x.Text != null)
             .OrderByDescending(y => y.Timestamp)
             .ToListAsync();
 
--- Ibis.Features/Messages/GetContent.cs
+++ Ibis.Features/Messages/GetContent.cs
@@ -2,19 +2,27 @@
 
 namespace Ibis.Features.Messages;
 
-public record GetContentRequest(string RoomId, string Tag, string Language);
+public record GetContentRequest(string RoomSlug, string Tag, string Language);
 public class GetContent : PublicFeature<GetContentRequest, GetContentResponse>
 {
-    public GetContent(IRepository<Message> messages)
+    public GetContent(IRepository<Message> messages, IRepository<Room> rooms)
     {
         Messages = messages;
+        Rooms = rooms;
     }
     public IRepository<Message> Messages { get; }
+    public IRepository<Room> Rooms { get; }
 
     public override async Task<GetContentResponse> ExecuteAsync(GetContentRequest request)
     {
+        var room = Rooms.Query.FirstOrDefault(x => x.Slug == request.RoomSlug);
+        if (room == null)
+        {
+            throw new NotFoundException($"Room {request.RoomSlug} not found");
+        }
+
         var message = await Messages.Query
-            .Where(x => x.RoomId == request.RoomId && x.Language == request.Language && x.Tag == request.Tag)
+            .Where(x => x.RoomId == room.Id && x.Language == request.Language && x.Tag == request.Tag)
             .OrderByDescending(y => y.Timestamp)
             .FirstOrDefaultAsync();
 
