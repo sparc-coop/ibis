adc1928b5d275be653d717fe17ff7f35c8a09e50
Joseph Young <joseph@josephayoung.com>
2022-10-26 12:12:48 -0500

    Fix sign in cookies

--- Ibis.Features/Startup.cs
+++ Ibis.Features/Startup.cs
@@ -42,6 +42,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        app.UseCookiePolicy();
         app.Sparcify<Startup>(env);
         app.UseEndpoints(endpoints => {
             endpoints.MapControllers();
