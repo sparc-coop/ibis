efd3582b79080362b4d5b93deb699795e35bf355
Joseph Young <joseph@josephayoung.com>
2022-11-11 11:04:12 -0600

    Pull up source message id check to improve performance

--- Ibis.Features/Messages/TranslateMessage.cs
+++ Ibis.Features/Messages/TranslateMessage.cs
@@ -15,14 +15,14 @@ public class TranslateMessage : RealtimeFeature<MessageTextChanged>
 
     public override async Task ExecuteAsync(MessageTextChanged notification)
     {
+        if (notification.Message.SourceMessageId != null) // Message already translated
+            return;
+
         var room = await Rooms.FindAsync(notification.Message.RoomId);
 
         if (room == null || notification.Message == null)
             throw new NotFoundException("Not found!");
 
-        if (notification.Message.SourceMessageId != null) // Message already translated
-            return;
-        
         var translatedMessages = await room.TranslateAsync(notification.Message, Translator);
 
         foreach (var translatedMessage in translatedMessages)
