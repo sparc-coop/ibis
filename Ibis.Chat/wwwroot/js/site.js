f42934d6a7df31d9784a130c6d87b6e776af97fd
Joseph Young <joseph@josephayoung.com>
2023-01-28 12:46:16 -0600

    Clean up unused js

--- Ibis.Chat/wwwroot/js/site.js
+++ Ibis.Chat/wwwroot/js/site.js
@@ -1,23 +1,4 @@
-﻿//File download
-async function downloadFileFromStream(fileName, contentStreamReference) {
-    const arrayBuffer = await contentStreamReference.arrayBuffer();
-    const blob = new Blob([arrayBuffer]);
-    const url = URL.createObjectURL(blob);
-
-    triggerFileDownload(fileName, url);
-
-    URL.revokeObjectURL(url);
-}
-
-function triggerFileDownload(url, fileName) {
-    const anchorElement = document.createElement('a');
-    anchorElement.href = url;
-    anchorElement.download = fileName ?? '';
-    anchorElement.click();
-    anchorElement.remove();
-}
-
-function speak(audio) {
+﻿function speak(audio) {
     const contentType = "audio/mp3";
     const sound = new Howl({
         src: [`data:${contentType};base64,${audio}`]
