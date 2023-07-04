0214cff767dc467b2026501ccb476818cf91945a
Joseph Young <joseph@josephayoung.com>
2023-07-04 12:03:47 -0500

    New Blossom extensions and multi-hosting setup

--- Ibis.Features/Program.cs
+++ Ibis.Features/Program.cs
@@ -1,15 +1,13 @@
 using Ibis;
+using Ibis._Plugins.Blossom;
 using Lamar.Microsoft.DependencyInjection;
 using Stripe;
-// using Sparc.Ibis;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseLamar();
 
-builder.AddBlossom(builder.Configuration["WebClientUrl"]);
-
-builder.Services
-        .AddCosmos<IbisContext>(builder.Configuration.GetConnectionString("Database")!, "ibis-prod", ServiceLifetime.Transient)
+var app = builder.Blossom<User>(s => 
+    s.AddCosmos<IbisContext>(builder.Configuration.GetConnectionString("Database")!, "ibis-prod", ServiceLifetime.Transient)
         .AddAzureStorage(builder.Configuration.GetConnectionString("Storage")!)
         .AddTwilio(builder.Configuration)
         .AddBlossomRealtime<IbisHub>()
@@ -17,36 +15,14 @@ builder.Services
         .AddScoped<ISpeaker, AzureSpeaker>()
         .AddScoped<IListener, AzureListener>()
         .AddSingleton<ExchangeRates>()
-        .AddScoped<GetAllContent>();
-
-builder.AddBlossomAuthentication<User>();
+        .AddScoped<GetAllContent>());
 
 // builder.Services.AddIbis(builder.Configuration["IbisApi"]!);
-builder.Services.AddServerSideBlazor();
-builder.Services.AddOutputCache();
-
-var app = builder.Build();
-
-app.UseBlazorFrameworkFiles();
-app.UseBlossom();
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToFile("index.html");
-
-if (builder.Environment.IsDevelopment())
-    app.UseDeveloperExceptionPage();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
+app.Host("ibis.chat", 5001, "Chat");
+app.Host("ibis.ink", 5002, "Ink");
 
 app.MapHub<IbisHub>("/hub");
-app.UseBlossomAuthentication<User>();
 app.UseAllCultures();
-
-// Warm up the entity framework model
-//_ = app.Services.GetRequiredService<IbisContext>().Model;
 
 StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"];
 
