8ed3fae211671385d27f2a11302a81c1b16871f1
Joseph Young <joseph@josephayoung.com>
2023-10-24 11:30:43 -0500

    Rip out Ibis Chat and replace with Ibis Ink

--- Ibis.Features/Program.cs
+++ Ibis.Features/Program.cs
@@ -2,6 +2,7 @@ using Ibis;
 using Lamar.Microsoft.DependencyInjection;
 using Stripe;
 using Sparc.Ibis;
+using Blazored.Modal;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseLamar();
@@ -18,6 +19,8 @@ builder.Services
         .AddScoped<IListener, AzureListener>()
         .AddSingleton<ExchangeRates>()
         .AddScoped<GetAllContent>();
+
+builder.Services.AddBlazoredModal();
 
 var auth = builder.Services.AddAzureADB2CAuthentication<User>(builder.Configuration);
 auth.AddCookie();
