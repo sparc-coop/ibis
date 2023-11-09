558f22e02e9b2e8b353d498d7f4e44cc7a790624
Joseph Young <joseph@josephayoung.com>
2023-11-09 06:59:20 -0600

    Apiset

--- Ibis.Features/_Plugins/Blossom/ApiSet.cs
+++ Ibis.Features/_Plugins/Blossom/ApiSet.cs
@@ -1,6 +1,10 @@
-﻿namespace Sparc.Blossom;
+﻿using Ardalis.Specification;
+
+namespace Sparc.Blossom;
 
 public class ApiSet<T>(IRepository<T> repository) where T : Entity
 {
+    IEnumerable<ISpecification<T>> Queries { get; }
+
 }
 
