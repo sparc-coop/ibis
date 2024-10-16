2d8ce6b27d774ddc2e289b2b239a1cdccee021e9
Laura Ferraz <lauraferraz5@gmail.com>
2024-10-16 16:51:39 -0300

    Update SearchContent code to return only the properties the UI needs

--- Ibis.Features/Rooms/Content/SearchContent.cs
+++ Ibis.Features/Rooms/Content/SearchContent.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Ibis.Rooms;
 
 public record SearchContentRequest(string SearchTerm);
-public record SearchContentResponse(List<Room> Rooms, List<Message> Messages);
+public record SearchContentResponse(List<RoomSummary> Rooms, List<MessageSummary> Message);
+public record RoomSummary(string Id, string Name, string Slug);
+public record MessageSummary(string Id, string RoomId, string Text, string Tag);
 
 public class SearchContent
 {
@@ -20,26 +23,28 @@ public class SearchContent
     {
         var searchTerm = request.SearchTerm;
 
-        var matchingRooms = await GetRoomBySlugAsync(searchTerm);
+        var matchingRooms = await GetRoomSummariesBySlugAsync(searchTerm);
 
-        var matchingMessages = await GetMessagesInAllRoomsAsync(searchTerm);
+        var matchingMessages = await GetMessageSummariesInAllRoomsAsync(searchTerm);
 
         return new SearchContentResponse(matchingRooms, matchingMessages);
     }
 
-    private async Task<List<Room>> GetRoomBySlugAsync(string searchTerm)
+    private async Task<List<RoomSummary>> GetRoomSummariesBySlugAsync(string searchTerm)
     {
         return await Rooms.Query
             .Where(x => x.Slug.Contains(searchTerm))
+            .Select(x => new RoomSummary(x.Id, x.Name, x.Slug)) 
             .ToListAsync();
     }
 
-    private async Task<List<Message>> GetMessagesInAllRoomsAsync(string searchTerm)
+    private async Task<List<MessageSummary>> GetMessageSummariesInAllRoomsAsync(string searchTerm)
     {
         return await Messages.Query
             .Where(x => (x.Text != null && x.Text.Contains(searchTerm)) ||
                         (x.Tag != null && x.Tag.Contains(searchTerm)))
             .OrderBy(y => y.Timestamp)
+            .Select(x => new MessageSummary(x.Id, x.RoomId, x.Text, x.Tag)) 
             .ToListAsync();
     }
 }
